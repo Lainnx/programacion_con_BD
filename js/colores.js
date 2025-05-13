74b040a1e2940deef54402f248705743acb981a2
das <tree@gmail.com>
2025-05-13 20:04:10 +0200

    2025/05/13

--- js/colores.js
+++ js/colores.js
@@ -8,7 +8,7 @@ formInsert.addEventListener("submit",(e)=>{
     document.getElementById("errorColor").textContent = "";
     //obtener los datos del formulario
     const usuario = formInsert["usuario"].value.trim();  // el input tiene name="usuario"
-    const color = formInsert["color"].value.trim();
+    const color = formInsert["color"].value.trim().toLocaleLowerCase();
     const nombre_que_no_se_corresponde_con_lo_que_hace = formInsert["nombre_que_no_se_corresponde_con_lo_que_hace"].value;
     const token = formInsert["token"].value;
 
@@ -28,4 +28,47 @@ formInsert.addEventListener("submit",(e)=>{
         document.getElementById("errorColor").textContent = mensajeError;
         return;
     }
+    //Comprobación del bot
+    if(!nombre_que_no_se_corresponde_con_lo_que_hace === ""){ // si web esta vacio es que esta bien
+        document.getElementById("errorColor").textContent = "Bot detectado";    // lo suyo seria crear otro parrafo con otro id
+        return;
+    }
+    //Comprobación por REGEX = Expresiones regulares
+    //https://www.w3schools.com/php/php_regex.asp
+    // Regla a cumplir
+    const regex1 = /^[A-zÑñÇçÁÉÍÓÚáéíóúÀÈÌÒÙàèìòùÄËÏÖÜäëïöü·\s]+$/
+    // Reglas a NO cumplir
+    const regex2 = /\W+/
+    const regex3 = /\d+/
+
+    const reglaUsuario = (regex2.test(usuario) || regex3.test(usuario)) && !regex1.test(usuario)    // si esto es tru MAL
+    const reglaColor = (regex2.test(color) || regex3.test(color)) && !regex1.test(color)        // regex.rest.(string a testear)
+
+    const mensaJeRegex ="Caracteres no válidos"
+    if (reglaUsuario || reglaColor){
+        document.getElementById("errorUsuario").textContent = mensaJeRegex;
+        document.getElementById("errorColor").textContent = mensaJeRegex;
+        return;
+    }
+    //Enviar datos a insert.php por POST
+    //necesitamos un objeto para enviar a php
+    const datos = new URLSearchParams();    // datos es el objeto
+    datos.append("usuario",usuario);    //mismos nombres que en el insert/update
+    datos.append("color",color);
+    datos.append("nombre_que_no_se_corresponde_con_lo_que_hace",nombre_que_no_se_corresponde_con_lo_que_hace);
+    datos.append("token",token);
+
+    fetch("../insert.php",{     // fetch tambien puede enviar datos, ../ porque esta en carpeta js
+        "method":"POST",        // estos nombres no te los puedes inventar, son los que son
+        "body":datos.toString(),    // enviamos los datos en forma de string
+        "headers":{
+                "Content-type":"application/x-www-form-urlencoded"      // hay diferentes tipos de contenido, jpg, json ...
+            }   //catch- negativo,then -positivo, finally-en cualquier caso
+    }).then(respuesta => respuesta.text())
+    .then(data => {
+        console.log(data);
+        location.reload(); // para recargar la pagina y se sincronize con BD en servidor
+    }).catch(error => {
+        console.log("Error: ", error);
+    })
 });
